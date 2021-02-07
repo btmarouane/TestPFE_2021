c5c5c09de3d5ded5af56b383983915378f64df63
marouane <btgmarouane@gmail.com>
2021-02-07 15:04:46 +0100

    test sii - Marouane Boutghanbout

--- src/main/java/sii/maroc/Vehicles.java
+++ src/main/java/sii/maroc/Vehicles.java
@@ -16,7 +16,7 @@ public class Vehicles {
 	
 	
 	/**
-	 * Constructor - Map description of gas consumptions to diesel, fuel, hybrid attributes
+	 * Constructor - Map description of gas consumption values to diesel, fuel, hybrid attributes
 	 * @param description
 	 */
 	public Vehicles(String description) {
@@ -32,55 +32,80 @@ public class Vehicles {
 
 
 	/**
-	 * @param type
+	 * @param vehicleType
 	 * @param gasType
 	 * @param closedDoors
 	 * @param distance
-	 * @return A String representing the car state
+	 * @return A String representing the vehicle state
 	 */
-	public String move(String type, String gasType, String closedDoors, String distance) {
-		Set<String> doorsSet = new HashSet<String>(Arrays.asList("1","2","3","4"));
+	public String move(String vehicleType, String gasType, String closedDoors, String distance) {
+
 		Set<String> closedDoorsSet = new HashSet<String>(Arrays.asList(closedDoors.split(" ")));
-		
+		Set<String> openDoorsSet = null;
 		String result = null;
 		float consumption = 0;
 		
-		if(type.equalsIgnoreCase("truck")) {
-			doorsSet.remove("3");
-			doorsSet.remove("4");
-		}
-		else if(type.equalsIgnoreCase("motorcycle")) {
-			doorsSet.removeAll(doorsSet);
-		}
+		openDoorsSet = getOpenDoors(vehicleType, closedDoorsSet);
 		
-		doorsSet.removeAll(closedDoorsSet);
-		
-		if(!doorsSet.isEmpty()) {
-			result = getCarShape(doorsSet);
+		if( !openDoorsSet.isEmpty() ) {
+			result = getCarShape(openDoorsSet);
 		}
 		
 		else {
-			result = "DOORS OK, MOVING. The "+type+" will consume ";
-			if(gasType.equals("Diesel")) {
-				consumption = getGasConsumption(distance, this.diesel);
-			}
-			else if(gasType.equals("Fuel")) {
-				consumption = getGasConsumption(distance, this.fuel);
-			}
-			else {
-				consumption = getGasConsumption(distance, this.hybrid);
-			}
-			
-			result += String.format("%.2f",consumption)+" L";
+			consumption = this.getGasConsumption(gasType, distance.split(" ")[0]);
+			result = "DOORS OK, MOVING. The "+
+					vehicleType+" will consume "+
+					String.format("%.2f",consumption)+" L";
 		}
 		
 		return result;
 	}
 
+
+	/**
+	 * @param gasType
+	 * @param distance 
+	 * @return Corresponding gas consumption in liter
+	 */
+	public float getGasConsumption(String gasType, String distance) {
+		float consumption = 0;
+		
+		if( gasType.equals("Diesel") ) {
+			consumption = Float.valueOf(Float.valueOf(distance)*this.diesel/100);
+		}
+		else if( gasType.equals("Fuel") ) {
+			consumption = Float.valueOf(Float.valueOf(distance)*this.fuel/100);
+		}
+		else {
+			consumption =Float.valueOf(Float.valueOf(distance)*this.hybrid/100);
+		}
+		
+		return consumption;
+	}
 		
 	/**
+	 * @param vehicleType
+	 * @param closedDoorsSet
+	 * @return Set containing open doors 
+	 */
+	static public Set<String> getOpenDoors(String vehicleType, Set<String> closedDoorsSet){
+		Set<String> openDoorsSet = new HashSet<String>(Arrays.asList("1","2","3","4"));
+		
+		if( vehicleType.equalsIgnoreCase("truck") ) {
+			openDoorsSet.remove("3");
+			openDoorsSet.remove("4");
+		}
+		else if( vehicleType.equalsIgnoreCase("motorcycle") ) {
+			openDoorsSet.removeAll(openDoorsSet);
+		}
+		openDoorsSet.removeAll(closedDoorsSet);
+		
+		return openDoorsSet;
+	}
+	
+	/**
 	 * @param gas consumption - for Example : "Diesel:5%"
-	 * @return percentage of gas consumption - for Example : 5%
+	 * @return percentage of gas consumption - for Example : 5
 	 */
 	static public float getGasConsumtionPercentage(String consumption) {
 		return Float.parseFloat(consumption.
@@ -89,23 +114,21 @@ public class Vehicles {
 	
 	
 	/**
-	 * @param distance - for Example : "200 KM"
-	 * @param gasConsumptionPercentage - for Example : 5%
-	 * @return gas consumption in liter
-	 */
-	static public float getGasConsumption(String distance, float gasConsumptionPercentage) {
-		return  Float.valueOf(Float.valueOf(distance.split(" ")[0])*gasConsumptionPercentage/100);
-	}
-	
-	/**
 	 * @param openDoors
 	 * @return A String shape indicating opened doors of the car
-	 */
+	 * Example :
+	 * /***********
+	 * DOORS KO, BLOCKED
+           _
+          | | 
+          /_|
+          
+        ************/
 	static public String getCarShape(Set<String> openDoors) {
 		String result = "DOORS KO, BLOCKED \n"+
 				"  _\n";
-		for(int i=1; i< 5; i++) {
-			if(openDoors.contains(String.valueOf(i))) {
+		for( int i=1 ; i< 5 ; i++ ) {
+			if( openDoors.contains(String.valueOf(i)) ) {
 				result += (i%2==0)?"\\\n":" /";
 			}
 			else {
